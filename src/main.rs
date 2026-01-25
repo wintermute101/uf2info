181757eaf4c5367d4844dd54cc9fea0096b15664
Maciej Skrzypek <maciek.skrzypek [at] gmail.com>
2026-01-25 17:54:27 +0100

    Add memory regions

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::io::prelude::*;
 use std::path::PathBuf;
 use clap::{Parser};
 use uftwo::Block;
+use std::fmt::Display;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -14,6 +15,47 @@ struct Cli {
     verbose: bool,
 
     outputfile: Option<PathBuf>,
+}
+
+struct MemoryRegions{
+    regions: Vec<Vec<u32>>
+}
+
+impl MemoryRegions {
+    fn new() -> Self{
+        MemoryRegions { regions: vec![] }
+    }
+
+    fn add_region(&mut self, new_start: u32, new_end: u32){
+        match match (
+            self.regions.binary_search_by(|v| v[1].cmp(&new_start)),
+            self.regions.binary_search_by(|v| v[0].cmp(&new_end)),
+        ) {
+            (Err(start), Err(end)) | (Ok(start), Err(end)) => (start, end),
+            (Err(start), Ok(end)) | (Ok(start), Ok(end)) => (start, end + 1),
+        } {
+            (start, end) => match match start != end {
+                true => vec![
+                    new_start.min(self.regions[start][0]),
+                    new_end.max(self.regions[end - 1][1]),
+                ],
+                false => vec![new_start, new_end],
+            } {
+                r => {
+                    self.regions.splice(start..end, vec![r]);
+                },
+            },
+        }
+    }
+}
+
+impl Display for MemoryRegions{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for i in self.regions.iter(){
+            f.write_fmt(format_args!("0x{:X}-0x{:X} ", i[0], i[1]))?;
+        }
+        Ok(())
+    }
 }
 
 fn main() -> Result<()> {
@@ -31,6 +73,8 @@ fn main() -> Result<()> {
     } else {
         None
     };
+
+    let mut regions = MemoryRegions::new();
 
     loop {
         let mut buf = [0; 512];
@@ -58,6 +102,10 @@ fn main() -> Result<()> {
 
         binary.extend(&buf[0..(block.data_len as usize)]);
 
+        if block.data_len > 0{
+            regions.add_region(block.target_addr, block.target_addr + block.data_len);
+        }
+
         total_blocks += 1;
     }
 
@@ -72,7 +120,7 @@ fn main() -> Result<()> {
 
     let filename = cli.inputfile.to_string_lossy();
 
-    println!("File {} len {} total blocks {total_blocks} binary len {}", filename, total_blocks*512, binary.len());
+    println!("File {}\nInput file size {} bytes\nTotal blocks {total_blocks}\nBinary len {} bytes\nMemory regions {}", filename, total_blocks*512, binary.len(), regions);
 
     Ok(())
 }
